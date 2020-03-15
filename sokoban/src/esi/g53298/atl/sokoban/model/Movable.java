f2c464928ddd9a6b56ccc0de1ce450a5440d4a15
Israel Meiresonne <israelmeiresonne97@gmail.com>
2020-03-15 21:10:57 +0100

    fix Movable enum

--- sokoban/src/esi/g53298/atl/sokoban/model/Movable.java
+++ sokoban/src/esi/g53298/atl/sokoban/model/Movable.java
@@ -8,8 +8,16 @@ public class Movable {
     private MovableType type;
     private final String SYMBOL;
     
+    /**
+     * Constructor
+     * @param type 
+     */
     public Movable(MovableType type){
         this.type = type;
         SYMBOL = type.getSymbol();
+    }
+    
+    public MovableType geType(){
+        return type;
     }
 }
