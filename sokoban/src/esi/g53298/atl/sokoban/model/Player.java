4ecbf3173f860f85ee55ef87829383f92918b91d
Israel Meiresonne <israelmeiresonne97@gmail.com>
2020-03-15 15:22:06 +0100

    add the Position class and its constructor

--- sokoban/src/esi/g53298/atl/sokoban/model/Player.java
+++ sokoban/src/esi/g53298/atl/sokoban/model/Player.java
@@ -5,5 +5,5 @@ package esi.g53298.atl.sokoban.model;
  * @author israelmeiresonne
  */
 public class Player extends Movable {
-    
+    private 
 }
---
+++ sokoban/src/esi/g53298/atl/sokoban/model/Position.java
@@ -0,0 +1,20 @@
+package esi.g53298.atl.sokoban.model;
+
+/**
+ *
+ * @author israelmeiresonne
+ */
+public class Position {
+    private int row;
+    private int column;
+    
+    /**
+     * Constructor
+     * @param row
+     * @param column 
+     */
+    public Position(int row, int column){
+        this.row = row;
+        this.column = column;
+    }
+}
