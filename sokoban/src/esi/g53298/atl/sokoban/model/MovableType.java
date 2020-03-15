0633cbaf8f16f337daa92882490917de179b08cf
Israel Meiresonne <israelmeiresonne97@gmail.com>
2020-03-15 20:56:28 +0100

    add enum MovableType

---
+++ sokoban/src/esi/g53298/atl/sokoban/model/MovableType.java
@@ -0,0 +1,23 @@
+package esi.g53298.atl.sokoban.model;
+
+/**
+ *
+ * @author israelmeiresonne
+ */
+public enum MovableType {
+    BOX("$"), Player("@");
+    
+    private String symbol;
+    
+    MovableType(String symbol){
+        this.symbol = symbol;
+    }
+    
+    /**
+     * 
+     * @return the movable symbol
+     */
+    public String getSymbol(){
+        return symbol;
+    }
+}
