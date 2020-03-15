bd5cf5e04e44190604f8b441b86353b32fb2d070
Israel Meiresonne <israelmeiresonne97@gmail.com>
2020-03-15 21:49:05 +0100

    add methods isWin, restart and giveUp in the Game class

--- sokoban/src/esi/g53298/atl/sokoban/model/Game.java
+++ sokoban/src/esi/g53298/atl/sokoban/model/Game.java
@@ -7,16 +7,41 @@ import java.io.FileNotFoundException;
  * @author israelmeiresonne
  */
 public class Game {
+
     private Maze maze;
-    
+
     /**
      * Constructor
+     *
      * @param level
-     * @throws FileNotFoundException 
+     * @throws FileNotFoundException
      */
-    public Game(int level) throws FileNotFoundException{
+    public Game(int level) throws FileNotFoundException {
         this.maze = new Maze(level);
     }
+
+    /**
+     * Check if the level is successful by check if all box is in a gaol square
+     *
+     * @return true if the level successful else false
+     */
+    public boolean isWin() {
+        return maze.isWin();
+    }
     
+    /**
+     * Restar the level by rebuilding the maze
+     * @throws FileNotFoundException 
+     */
+    public void restarLevel() throws FileNotFoundException{
+        maze.restarLevel();
+    }
     
+    /**
+     * Allow to give up the game
+     */
+    public void giveUp(){
+        maze = null;
+    }
+
 }
